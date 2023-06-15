97133b519dd013d16e3b071c959b922e2e46959c
AlvinOkoti <alvin.okoti99@gmail.com>
2023-06-15 11:26:35 -0700

    using puts command

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-comments with puts for output
+ *Return:0(successful)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
