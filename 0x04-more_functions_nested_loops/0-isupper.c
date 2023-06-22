d920521b790013154f64d33e515a8f435521077b
AlvinOkoti <alvin.okoti99@gmail.com>
2023-06-22 13:27:41 -0700

    2-mul.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,14 @@
 #include "main.h"
 
 /**
- * main - checks for letters
- * _isupper - uppercase letters
- * @c: char to check
+ * main - entry point
+ *
+ *  _isupper - uppercase letters
+ *  @c: char to check
+ *
+ *  Description: This function takes a character and checks if it is an
+ *  uppercase letter in the ASCII character set. It returns 1 if the
+ *  character is uppercase, and 0 otherwise.
  *
  * Return: 0 or 1
  */
